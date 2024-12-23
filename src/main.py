4581c8ba510a258d0004e77e6278067a8749e2aa
Stephane Gagnon <stephane-gagnon@hotmail.com>
2024-12-23 18:29:19 -0500

    refactor: Remove the osc observer thread

--- src/main.py
+++ src/main.py
@@ -5,19 +5,10 @@
 
 import os
 import json
-import time
 import asyncio
 from services.logic import LogicService
 
-# from services.live import LiveContext
-from fastapi import (
-    Request,
-    FastAPI,
-    WebSocket,
-    BackgroundTasks,
-    WebSocketException,
-    WebSocketDisconnect,
-)
+from fastapi import Request, FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -35,9 +26,6 @@ class ConnectionManager:
 
     async def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
 
     async def broadcast(self, payload):
         for connection in self.active_connections:
@@ -58,35 +46,19 @@ with open(config) as FILE:
 
 
 """ Mididings and OSC context """
-logic = None
 logic = LogicService(configuration["osc_server"])
-
-
-async def osc_observer_thread():
-    while True:
-        if await logic.is_dirty():
-            await mididings_context_update()
-        await manager.broadcast(
-            {"action": "on_start" if await logic.is_running() else "on_exit"}
-        )
-        await asyncio.sleep(0.125)
 
 
 async def mididings_context_update():
     await logic.set_dirty(False)
-    await manager.broadcast({
-        "action": "mididings_context_update",
-        "payload" : logic.scene_context.payload
-    })
+    await manager.broadcast(
+        {"action": "mididings_context_update", "payload": logic.scene_context.payload}
+    )
 
 
-async def get_mididings_context():
-    await mididings_context_update()
-
-
-'''
+"""
     REST API endpoints
-'''
+"""
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -95,23 +67,14 @@ async def index(request: Request):
 
 
 @app.get("/ui", response_class=HTMLResponse)
-async def ui(request: Request, background_tasks: BackgroundTasks):
-    if not background_tasks.tasks:
-        print("Create observer")
-        background_tasks.add_task(osc_observer_thread)
-
+async def ui(request: Request):
     return templates.TemplateResponse(
         name="ui.html" if logic.scene_context.scenes else "no_context.html",
         context={"request": request},
     )
 
 
-"""
-    REST API endpoints
-"""
-
-
-@app.get("/api/")
+@app.get("/api/", status_code=204)
 async def api(action: str, payload: int = 0):
     if action in mididings_actions:
         (
@@ -119,10 +82,10 @@ async def api(action: str, payload: int = 0):
             if payload == 0
             else await mididings_actions[action](payload)
         )
-    return "", 204
+    return ""
 
 
-""" Websockets handler """
+""" Websocket handler """
 
 
 @app.websocket("/ws")
@@ -130,47 +93,42 @@ async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     try:
         while True:
-            data = await websocket.receive_json()
-
-            action = data["action"]
-
-            if action in mididings_actions:
-                (
-                    await mididings_actions[action]()
-                    if not "payload" in data
-                    else await mididings_actions[action](int(data["payload"]))
-                )
-
-            if action in logic_actions:
-                await logic_actions[action](websocket)
+            # Send status periodic task
+            await manager.broadcast(
+                {"action": "on_start" if await logic.is_running() else "on_exit"}
+            )
 
             if await logic.is_dirty():
-                logic_actions["mididings_context_update"]
+                await logic_actions["mididings_context_update"]()
 
+            try:
+                # Handle incoming messages from the client
+                data = await asyncio.wait_for(websocket.receive_json(), timeout=0.125)
+                action = data["action"]
+
+                if action in mididings_actions:
+                    (
+                        await mididings_actions[action]()
+                        if not "payload" in data
+                        else await mididings_actions[action](int(data["payload"]))
+                    )
+
+                if action in logic_actions:
+                    await logic_actions[action](websocket)
+
+            except asyncio.TimeoutError:
+                # No message received during the timeout, continue the loop
+                pass
     except WebSocketDisconnect:
         await manager.disconnect(websocket)
-
     except asyncio.exceptions.CancelledError:
         print("------------------------CancelledError")
-
     finally:
         print("exit")
 
 
-""" API calls  """
-
-
-async def on_refresh(websocket: WebSocket):
-    # This is mandatory to let time for mididings to process changes before re-render
-    await asyncio.sleep(0.125)
-    await manager.broadcast(
-        {"action": "on_refresh", "payload": logic.scene_context.payload}
-    )
-    await logic.set_dirty(False)
-
-
 async def on_quit(websocket: WebSocket):
-    await websocket.send_json(
+    await manager.broadcast(
         {
             "action": "on_terminate",
         }
@@ -192,13 +150,12 @@ mididings_actions = {
     "next_subscene": logic.next_subscene,
     "prev_subscene": logic.prev_subscene,
     "switch_subscene": logic.switch_subscene,
-    "get_mididings_context" : get_mididings_context,
+    "get_mididings_context": mididings_context_update,
 }
 
 
 logic_actions = {
     "on_connect": on_connect,
-    "on_refresh": on_refresh,
     "quit": on_quit,
-    "mididings_context_update" : mididings_context_update
+    "mididings_context_update": mididings_context_update,
 }
