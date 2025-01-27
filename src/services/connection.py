9c05b945d3bf1402eccd07e33a420b3d25284f12
Stephane Gagnon <stephane-gagnon@hotmail.com>
2025-01-26 20:41:11 -0500

    refactor: Robust connection manager

--- src/services/connection.py
+++ src/services/connection.py
@@ -4,19 +4,31 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 from fastapi import  WebSocket
-
+from typing import List
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        if websocket not in self.active_connections:
+            print(f"Connecting: {websocket.client}")
+            self.active_connections.append(websocket)
 
-    async def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, websocket: WebSocket):
+        try:
+            if websocket in self.active_connections:
+                print(f"Disconnecting: {websocket.client}")
+                self.active_connections.remove(websocket)
+        except:
+            print("Tried to remove a connection that is not in the list.")
 
-    async def broadcast(self, payload):
-        for connection in self.active_connections:
-            await connection.send_json(payload)
+    async def broadcast(self, message: dict):
+        print(f"Broadcasting to {len(self.active_connections)} connections")
+        for websocket in self.active_connections[:]:
+            try:
+                await websocket.send_json(message)
+            except Exception as e:
+                print(f"WebSocket error: {e} for {websocket.client}")
+                self.disconnect(websocket)
